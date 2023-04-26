1694d096e7398e73f7ab16d1e1f73fb56ea6f2c4
Abigail <ashbella333@gmail.com>
2023-04-26 13:13:02 +0000

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <div id="app">
-    <h1 class="bg-red-300">helloooooooooooooooooo</h1>
+  <div>
+    <h1 class="text-blue-300 w-10">helloooooooooooooooooo</h1>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+//  import ValidateForm from './components/ValidateForm.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    // ValidateForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -1,20 +1,16 @@
 <template>
-  <div class="hello">
-   
-    
-  </div>
+    <div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'ValidateForm',
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
