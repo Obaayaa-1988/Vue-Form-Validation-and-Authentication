a1e18b1e5f096fa4443785a748035a58f49bda88
Abigail <ashbella333@gmail.com>
2023-04-26 11:25:36 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="bg-red-300">helloooooooooooooooooo</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"], //like this
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
